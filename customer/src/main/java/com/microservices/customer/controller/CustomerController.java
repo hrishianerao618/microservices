141f2778ac7786bec8c897d01a5dda8e3e6d9c38
Hrishikesh Anerao <hrishianerao618@gmail.com>
2023-08-20 23:31:20 +0530

    circuitbreaker and timelimiter handling

--- customer/src/main/java/com/microservices/customer/controller/CustomerController.java
+++ customer/src/main/java/com/microservices/customer/controller/CustomerController.java
@@ -3,16 +3,22 @@ package com.microservices.customer.controller;
 import com.microservices.customer.dto.CustomerDTO;
 import com.microservices.customer.dto.RentalDTO;
 import com.microservices.customer.service.CustomerService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.timelimiter.annotation.TimeLimiter;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
 
 @RestController
 @RequestMapping("/api/microservice/customer")
+@Slf4j
 public class CustomerController {
 
     @Autowired
@@ -23,8 +29,20 @@ public class CustomerController {
     }
 
     @GetMapping(path = "/getAllRentalsByCustomer/{customerId}")
-    public List<RentalDTO> getAllRentalsByCustomer(@PathVariable Long customerId){
-       return  customerService.getRentalsForCustomer(customerId);
+    @CircuitBreaker(name = "rental", fallbackMethod = "rentalCallFailed")
+    @TimeLimiter(name = "rental",fallbackMethod = "rentalCallFailedTimer")
+    public CompletableFuture<List<RentalDTO>> getAllRentalsByCustomer(@PathVariable Long customerId){
+       return  CompletableFuture.supplyAsync(()->customerService.getRentalsForCustomer(customerId));
+    }
+
+    public CompletableFuture<List<RentalDTO>> rentalCallFailed( Long customerId , RuntimeException exception){
+        log.error("rentalCallFailed::{}", exception);
+        return CompletableFuture.supplyAsync(()->new ArrayList<>());
+    }
+
+    public CompletableFuture<List<RentalDTO>> rentalCallFailedTimer( Long customerId , RuntimeException exception){
+        log.error("rentalCallFailed::{}", exception);
+        return CompletableFuture.completedFuture(new ArrayList<>());
     }
 
 }
--- rental/src/main/java/com/microservices/rental/service/impl/RentalServiceImpl.java
+++ rental/src/main/java/com/microservices/rental/service/impl/RentalServiceImpl.java
@@ -8,6 +8,7 @@ import com.microservices.rental.entity.Rental;
 import com.microservices.rental.entity.Staff;
 import com.microservices.rental.repository.RentalRepository;
 import com.microservices.rental.service.RentalService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,9 +16,11 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
+@Slf4j
 public class RentalServiceImpl implements RentalService {
     @Autowired
-    RentalRepository rentalRepository ;
+    RentalRepository rentalRepository;
+
     @Override
     public List<RentalDTO> getAllRentals() {
         return rentalRepository.findAll().stream().map(this::convertToDTO).collect(Collectors.toList());
@@ -25,12 +28,20 @@ public class RentalServiceImpl implements RentalService {
 
     @Override
     public List<RentalDTO> findByCustomerId(Long customerId) {
-        return rentalRepository.findByCustomerId(customerId).stream()
-                .map(this::convertToDTO)
-                .collect(Collectors.toList());
+        log.info("Wait Starts");
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        } finally {
+            log.info("Wait Ends");
+            return rentalRepository.findByCustomerId(customerId).stream()
+                    .map(this::convertToDTO)
+                    .collect(Collectors.toList());
+        }
     }
 
-    private RentalDTO convertToDTO(Rental rental){
+    private RentalDTO convertToDTO(Rental rental) {
         RentalDTO rentalDTO = new RentalDTO();
 
         rentalDTO.setRentalId(rental.getRentalId());
@@ -46,7 +57,7 @@ public class RentalServiceImpl implements RentalService {
         return rentalDTO;
     }
 
-    private InventoryDTO convertToInventoryDTO(Inventory inventory){
+    private InventoryDTO convertToInventoryDTO(Inventory inventory) {
         InventoryDTO inventoryDTO = new InventoryDTO();
         inventoryDTO.setInventoryId(inventory.getInventoryId());
         inventoryDTO.setStoreId(inventory.getStoreId());
@@ -54,7 +65,8 @@ public class RentalServiceImpl implements RentalService {
         inventoryDTO.setLastUpdate(inventory.getLastUpdate());
         return inventoryDTO;
     }
-    private StaffDTO convertToStaffDTO(Staff staff){
+
+    private StaffDTO convertToStaffDTO(Staff staff) {
         StaffDTO staffDTO = new StaffDTO();
         staffDTO.setStaffId(staff.getStaffId());
         staffDTO.setActive(staff.getActive());
